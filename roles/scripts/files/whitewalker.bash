429588c2f332d2735ac50bde6395a12ca9f09d93
Thane Thomson <connect@thanethomson.com>
2019-09-12 18:20:22 -0400

    Update GoT for tm-load-test v0.6.2
    Updates the relevant files/configuration to be able to support
    `tm-load-test` v0.6.2 (the latest at this point in time). This updates
    the `tm-load-test` configuration to work by way of command line
    parameters instead of a configuration file, and takes the values of
    these command line parameters from each experiment's `config.toml` file.

--- roles/scripts/files/whitewalker.bash
+++ roles/scripts/files/whitewalker.bash
@@ -12,12 +12,25 @@ fi
 export XP="${EXPERIMENTS}"
 log experiment 0
 
+# Configure this node
+export WHITEWALKER_ID="whitewalker${ID}"
+# Set up and run tm-load-test master - wait until it finishes
+if [ -f /etc/experiments/"${XP}"/config.toml ]; then
+  # We run stemplate twice to allow for double variable interpolation - 
+  # allows us to refer to variables from within the `config.toml` file too
+  stemplate /usr/local/sbin/tm-load-test-slave.bash --env -f /etc/experiments/"${XP}"/config.toml -o /home/tm-load-test
+else
+  stemplate /usr/local/sbin/tm-load-test-slave.bash --env -o /home/tm-load-test
+fi
+chown tm-load-test /home/tm-load-test/tm-load-test-slave.bash
+chmod +x /home/tm-load-test/tm-load-test-slave.bash
+
 # Set up tm-load-test
 trap 'log whitewalker 10' ERR
-log "whitewalker{$ID}" 1
-stemplate /etc/experiments/"${XP}"/load-test.toml -o /home/tm-load-test/load-test.toml --env -f /etc/experiments/"${XP}"/config.toml
-chown tm-load-test /home/tm-load-test/load-test.toml
-sudo -u tm-load-test tm-load-test -slave -c /home/tm-load-test/load-test.toml > "/home/tm-load-test/tm-load-test-$(date +%Y%m%h-%H%M%S).log"
+log "${WHITEWALKER_ID}" 1
 
-log "whitewalker${ID}" 0
+sudo -u tm-load-test \
+  /home/tm-load-test/tm-load-test-slave.bash
+
+log "${WHITEWALKER_ID}" 0
 trap '' ERR
